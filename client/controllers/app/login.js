9d1b3bceb81cf4cbbf64a2c0b58ac7c883d2bc93
selvabharathi <selvabharathi.p@flex.com>
2018-01-03 03:08:52 -0800

    [Studio] Modified screen "login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -1,7 +1,7 @@
 angular.module('app').controller('app_login', app_login);
 function app_login($scope, app, $q) {
     'use strict';
-    console.log('data objects -->', $scope.data);
+    //console.log('data objects -->', $scope.data);
     app.init($scope);
     if (!$scope.data) {
         $scope.data = {};
