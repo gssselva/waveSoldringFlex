384962052f985ad610223779208ecaad5af82c44
selvabharathi <selvabharathi.p@flex.com>
2018-01-03 03:08:29 -0800

    [Studio] Modified screen "sitepage"

--- client/controllers/app/sitepage.js
+++ client/controllers/app/sitepage.js
@@ -2,5 +2,5 @@ angular.module('app').controller('app_sitepage', app_sitepage);
 function app_sitepage($scope, app) {
     'use strict';
     app.init($scope);
-console.log("page-->"+page.data);
+console.log("page-->"+$scope.data);
 }
