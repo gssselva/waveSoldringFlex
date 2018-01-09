e0e11a447d8d08d69ff36623c312057b07db57bf
selvabharathi <selvabharathi.p@flex.com>
2018-01-09 04:08:11 -0800

    [Studio] Modified screen "dashboard"

--- client/controllers/app/dashboard.js
+++ client/controllers/app/dashboard.js
@@ -2,4 +2,9 @@ angular.module('app').controller('app_dashboard', app_dashboard);
 function app_dashboard($scope, app) {
     'use strict';
     app.init($scope);
+    $scope.getDetails = function (data,companyCode) {
+     console.log("selected item-->"+data);
+    //var parameter = {'username': $scope.data.username, 'password': $scope.data.password};
+    app.call('wavesoldering_methods.getDownTimeDetails', parameter);
+    };
 }
