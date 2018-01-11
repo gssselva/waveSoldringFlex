67406f283ab0ce0025de4405f0fad4590e35a646
selvabharathi <selvabharathi.p@flex.com>
2018-01-10 22:45:26 -0800

    [Studio] Modified screen "dashboard", Modified screen "sitepage"

--- client/controllers/app/dashboard.js
+++ client/controllers/app/dashboard.js
@@ -1,7 +1,11 @@
 angular.module('app').controller('app_dashboard', app_dashboard);
 function app_dashboard($scope, app) {
     'use strict';
-    app.init($scope);
+    app.init($scope,function(){
+        //app.call('wavesoldering_methods.getCompanyByCode', parameter);
+     console.log("in dash init");
+    });
+  
     $scope.getDetails = function (data,companyCode) {
      
     var parameter = {'selectedItem': data, 'CompanyCode': companyCode};
--- client/controllers/app/sitepage.js
+++ client/controllers/app/sitepage.js
@@ -2,4 +2,7 @@ angular.module('app').controller('app_sitepage', app_sitepage);
 function app_sitepage($scope, app) {
     'use strict';
     app.init($scope);
+    $scope.preferedSite = function(item){
+        app.call('wavesoldering_methods.getCompanyByCode', item.CompanyCode);
+    }
 }
