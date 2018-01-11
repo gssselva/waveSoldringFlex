3e98c839c4df59f1ddaed02962c6471717d6647d
selvabharathi <selvabharathi.p@flex.com>
2018-01-11 01:28:34 -0800

    [Studio] Modified screen "details", Modified screen "dashboard"

--- client/controllers/app/details.js
+++ client/controllers/app/details.js
@@ -2,4 +2,7 @@ angular.module('app').controller('app_details', app_details);
 function app_details($scope, app) {
     'use strict';
     app.init($scope);
+    var commentsDisplay = function(){
+        console.log("comments clicked...");
+    }
 }
