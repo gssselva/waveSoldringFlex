7766c1ac1cfd4c74473902d778de1a9bb70ba161
selvabharathi <selvabharathi.p@flex.com>
2018-01-17 01:57:05 -0800

    [Studio] Modified screen "login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -1,12 +1,15 @@
 angular.module('app').controller('app_login', app_login);
-function app_login($scope, app, $q,data) {
+function app_login($scope, app, $q) {
     'use strict';
     //console.log('data objects -->', $scope.data);
-    app.init($scope);
+    app.init($scope,function(data){
+        
+    
     if (!$scope.data) {
         $scope.data = {};
         
     }
+    });
     var checkSupport = function () {
         var deferred = $q.defer();
         if (typeof cordova !== 'undefined' && window.plugins && window.plugins.touchid) {
@@ -57,4 +60,4 @@ function app_login($scope, app, $q,data) {
         window.plugins.touchid.save('credentials', JSON.stringify(credentials));
         $scope.doLogin(credentials, true);
     };
-}
+ }
