a7d4177c76507518fa65ef78d24a0ba8cd831be8
selvabharathi <selvabharathi.p@flex.com>
2018-01-17 02:52:00 -0800

    [Studio] Modified screen "sitepage"

--- client/controllers/app/sitepage.js
+++ client/controllers/app/sitepage.js
@@ -2,7 +2,11 @@ angular.module('app').controller('app_sitepage', app_sitepage);
 function app_sitepage($scope, app) {
     'use strict';
     var selectedCompany;
-    app.init($scope);
+    
+    app.init($scope,function(data){
+        //app.call('wavesoldering_methods.getCompanyByCode', parameter);
+     console.log("in site init"+data);
+    });
     $scope.preferedSite = function(item){
         //app.call('wavesoldering_methods.getCompanyByCode', item.CompanyCode);
         selectedCompany = item;
