ef9d4ed3d166ab69457e7247ccc5e7294339cd9d
selvabharathi <selvabharathi.p@flex.com>
2018-01-18 21:34:29 -0800

    [Studio] Modified screen "details"

--- client/controllers/app/details.js
+++ client/controllers/app/details.js
@@ -5,4 +5,22 @@ function app_details($scope, app) {
     $scope.commentsDisplay = function(){
         console.log("comments clicked...");
     }
+    $scope.data.searchDeatils ='';
+    $scope.customDetailsSearch = function (item) {
+        if (!$scope.data.searchDeatils) {
+            return true;
+        }
+        var matched = true;
+        $scope.data.searchDeatils.split(' ').forEach(function (token) {
+            matched = matched && match(item, escapeRegExp(token));
+        });
+        return matched;
+    };
+    function escapeRegExp(token) {
+        return token.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+    }
+    var match = function (item, val) {
+        var regex = new RegExp(val, 'i');
+        return item.ReasonDescription.toLowerCase().search(regex) >= 0;//search is made based on ReasonDescription
+    };
 }
