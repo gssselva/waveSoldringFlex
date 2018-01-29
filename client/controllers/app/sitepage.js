9b04dfa68b4663e3d6933202af5c17ce6f4aa1a6
selvabharathi <selvabharathi.p@flex.com>
2018-01-28 22:56:34 -0800

    [Studio] Modified screen "sitepage"

--- client/controllers/app/sitepage.js
+++ client/controllers/app/sitepage.js
@@ -1,5 +1,5 @@
 angular.module('app').controller('app_sitepage', app_sitepage);
-function app_sitepage($scope, app) {
+function app_sitepage($scope, app,$ionicloading) {
     'use strict';
     app.init($scope,function(data){
     //     var adid ='gssselva';//need to replace static value with localstorage value.
@@ -30,6 +30,7 @@ function app_sitepage($scope, app) {
     $scope.preferedSite = function(item){
         //app.call('wavesoldering_methods.getCompanyByCode', item.CompanyCode);
         selectedCompany = item;
+        $scope.app.showLoading("sample--");
     }
     $scope.defaultSite = function(){
         //app.call('wavesoldering_methods.getCompanyByCode', selectedCompany.CompanyCode);
