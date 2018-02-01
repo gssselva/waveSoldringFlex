db2ce56943650c508794488da87c3193222df6b5
selvabharathi <selvabharathi.p@flex.com>
2018-01-31 21:43:05 -0800

    [Studio] Add screen "comments"

---
+++ client/controllers/app/comments.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_comments', app_comments);
+function app_comments($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -69,5 +69,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.comments', {
+        views: {
+            app: {
+                controller: 'app_comments',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.comments');
+                }
+            }
+        }
     });
 });
