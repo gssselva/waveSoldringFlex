3560a49294311542540cd715e79c5a70995ce93c
gssmages <32328556+gssmages@users.noreply.github.com>
2018-02-01 15:14:00 +0530

    commit

--- server/api/login.js
+++ server/api/login.js
@@ -3,36 +3,6 @@ var config = require('../config');
 var browser =  require('powwow-server-common').browser;
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-// exports.loginBasic = function (page, params) {
-//     var loadFailed = false;
-//     page.onResourceLoad(config.loginURL).then(function (response) {
-//         if (response.contentType == null && response.status == null && response.headers.length == 0) {
-//             loadFailed = true;
-//         }
-//     });
-	
-// 	//onLoadfailure accepts regular Expression to detect error
-//     page.onLoadFailure(/about:corlogin/)
-//         .data(function (data) {
-//             if (loadFailed) {
-//                 data.error = "Unable to access " + config.loginURL;
-//             } else {
-//                 data.error = "Login failed.  Please check your username and password and try again.";
-//             }
-//             data.username = params.username;
-//         })
-//         .screen('login');
-
-//     browser.getMainPage().set('settings.userName', params.username, function () {
-//         browser.getMainPage().set('settings.password', params.password, function () {
-//             browser.getMainPage().open(config.loginURL);
-//         });
-//     });
-
-// }
-
-
-
 exports.login = function (page,params) {
     var xmlhttp = new XMLHttpRequest();
     var url = "http://sacnte245.americas.ad.flextronics.com:9094/api/Authentication/Authentication";
@@ -40,13 +10,26 @@ exports.login = function (page,params) {
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(this.responseText);
+            console.log("response-->"+response.IsSuccess);
+            console.log("IsDefaultCompany-->"+response.IsDefaultCompany);
+            if (response.IsSuccess == 'true' && +response.IsDefaultCompany == 'true'){
+                //valid user with default site preference
+                defaultSitePreference(response.CompanyCode);
+            }else if(response.IsSuccess == 'true' && +response.IsDefaultCompany == 'false'){
+                //valid user without default site preference
+                sitePreference();
+            }else if(response.IsSuccess == 'false' && +response.IsDefaultCompany == 'false'){
+                //invlaid user
+                invalidUser();
+            }
             // for (var i = 0; i < response.length; i++) {
             //     reqResponse[i] = {'name': response[i].name,'email':response[i].email,'phone':response[i].phone,'username':response[i].username,'website':response[i].website};
             // }
         }
     };
     xmlhttp.open("POST", url, false);
-    xmlhttp.send(params);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify(params));
 
     //Is the below method is used to pass the response to the next page?
     // page.data(function(data) {
@@ -54,7 +37,12 @@ exports.login = function (page,params) {
     // })
     //     .screen("result");
 }
+exports.defaultSitePreference = function(CompanyCode){
 
+}
+exports.SitePreference = function(){
+    
+    }
 // exports.resultdetails = function(page, param) {
 //     // the details rest call will go here
 //     page.data(function(data) {
