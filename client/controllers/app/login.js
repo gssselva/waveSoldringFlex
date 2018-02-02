93845f29607ea56359156691b69db16f29bb4c19
selvabharathi <selvabharathi.p@flex.com>
2018-02-01 21:34:20 -0800

    [Studio] Modified screen "login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -9,55 +9,12 @@ function app_login($scope, app, $q, $localStorage) {
         
     }
     });
-    var checkSupport = function () {
-        var deferred = $q.defer();
-        if (typeof cordova !== 'undefined' && window.plugins && window.plugins.touchid) {
-            window.plugins.touchid.isAvailable(function () {
-                window.plugins.touchid.has('credentials', function () {
-                    deferred.resolve(true);
-                }, function () {
-                    deferred.resolve(false);
-                });
-            });
-        }
-        return deferred.promise;
-    };
-    $scope.loginViaTouch = function () {
-        checkSupport().then(function (isAvailable) {
-            if (isAvailable) {
-                window.plugins.touchid.verify('credentials', ' ', function (stringCreds) {
-                    var credentials = JSON.parse(stringCreds);
-                    $scope.data.username = credentials.username;
-                    $scope.data.password = credentials.password;
-                    $scope.$apply();
-                    $scope.doLogin(credentials);
-                }, function (err) {
-                    alert(err);
-                });
-            } else {
-                alert('touch id is not available');
-            }
-        });
-    };
-    $scope.reset = function () {
-        localStorage.clear();
-    };
+    
     $scope.login = function () {
     var credentials = {"UserName":$scope.data.username,
              "PassWord":$scope.data.password};
             $localStorage.UserName = $scope.data.username;
     app.call('login.login', credentials);
     };
-    $scope.doLogin = function (credentials, useWebsocket) {
-        $scope.app.showLoading('Logging in');
-        var username = credentials.username;
-        var password = credentials.password;
-        if (useWebsocket || app.login($scope.data.username, $scope.data.password)) {
-            app.action('login', 'submit', this);
-        }
-    };
-    $scope.doAppLogin = function (credentials) {
-        window.plugins.touchid.save('credentials', JSON.stringify(credentials));
-        $scope.doLogin(credentials, true);
-    };
+
  }
