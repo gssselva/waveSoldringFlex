37396aec53ffe5b63a53ecd06bf7135a82c0cfd7
selvabharathi <selvabharathi.p@flex.com>
2018-03-06 05:18:05 -0800

    [Studio] Modified screen "wavesoldringflex"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -1,9 +1,9 @@
 angular.module('app').controller('app', app);
 function app($scope, app, $localStorage) {
     'use strict';
-    var comp_code = $localStorage.CompanyCode; 
+    
      $scope.dashboardCall = function(){
-     
+     var comp_code = $localStorage.CompanyCode;
      app.call('wavesoldering_methods.getCompanyByCode', comp_code);
  }
 }
