ef767356ce1cb180d2aa3579223be12725a1f685
selvabharathi <selvabharathi.p@flex.com>
2018-03-13 02:31:04 -0700

    [Studio] Modified screen "wavesoldringflex"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -1,9 +1,24 @@
 angular.module('app').controller('app', app);
-function app($scope, app, $localStorage) {
+function app($scope, app, $localStorage,$ionicPopup) {
     'use strict';
     
      $scope.dashboardCall = function(){
      var comp_code = $localStorage.CompanyCode;
      app.call('wavesoldering_methods.getCompanyByCode', comp_code);
+ }
+ $scope.log_out = function(){
+     var confirmPopup = $ionicPopup.confirm({
+             title: 'Logout',
+             template: 'Are you sure you want to logout?'
+             });
+                 
+         confirmPopup.then(function(res) {
+            if(res) {
+                 console.log('yes logout!');
+                 app.logout();
+                } else {
+                    console.log('no log_out!');
+                }
+            });
  }
 }
