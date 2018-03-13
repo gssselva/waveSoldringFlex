2d149ac03a88bf97bfc7c5e31efb7a8faa3820a4
selvabharathi <selvabharathi.p@flex.com>
2018-03-13 03:10:49 -0700

    [Studio] Modified screen "menu"

--- client/controllers/app/menu.js
+++ client/controllers/app/menu.js
@@ -7,5 +7,7 @@ function app_menu($scope, app, $localStorage) {
 //      var comp_code = $localStorage.CompanyCode
 //      app.call('wavesoldering_methods.getCompanyByCode', comp_code);
 //  }
-
+$scope.log_out = function(){
+     app.logout();
+ }
 }
